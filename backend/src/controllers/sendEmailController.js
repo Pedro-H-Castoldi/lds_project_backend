9d7326a18cef157aad1673f3382a14bae43264d2
Pedro-H-Castoldi <pedrohenriquecastoldi.b@hotmail.com>
2021-04-06 13:19:44 -0300

    Login fixed

--- backend/src/controllers/sendEmailController.js
+++ backend/src/controllers/sendEmailController.js
@@ -1,6 +1,6 @@
 const sgMail = require('@sendgrid/mail')
 
-const API_KEY = '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'
+const API_KEY = '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'
 
 module.exports = {
     async create(request, response) {
@@ -10,20 +10,19 @@ module.exports = {
 
         const message = {
             to: email,
-            from: 'kaio.andersonr@gmail.com',
+            from: 'pedrohenriquecastoldi.b@hotmail.com',
             subject: 'Senha de acesso',
             text: `Aqui está sua senha para acessar o sistema do Eclipse NET: ${password}`,
             html: `<p>Aqui está sua senha para acessar o sistema do Eclipse NET: ${password}</p>`
         }
 
-        
 
         try {
             sgMail.send(message)
             return response.json({ success: true})
 
         } catch (error) {
-            return response.json(message)
+            return response.json(message.error)
         }
 
         
